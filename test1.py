a32ca97a63376da371b4e160e019689b17401c79
3150840539@qq.com <3150840539@qq.com>
2024-03-21 22:10:12 +0800

    message 4: First unit test

--- test1.py
+++ test1.py
@@ -1,16 +1,16 @@
-from selenium import webdriver
-import unittest
+# from selenium import webdriver
+# import unittest
 
-class NewVisitorTest(unittest.TestCase):
-    def setUp(self):
-        self.browser = webdriver.Chrome()
+# class NewVisitorTest(unittest.TestCase):
+#     def setUp(self):
+#         self.browser = webdriver.Chrome()
 
-    def tearDown(self):
-        self.browser.quit()
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do',self.browser.title),"browser title was:" + self.browser.title
-        self.fail('Finish the test!')
+#     def tearDown(self):
+#         self.browser.quit()
+#     def test_can_start_a_list_and_retrieve_it_later(self):
+#         self.browser.get('http://localhost:8000')
+#         self.assertIn('To-Do',self.browser.title),"browser title was:" + self.browser.title
+#         self.fail('Finish the test!')
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
