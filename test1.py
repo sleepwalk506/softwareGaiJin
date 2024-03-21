b8def9db732d54095bf1f21ffdd26f7d44f69a49
3150840539@qq.com <3150840539@qq.com>
2024-03-21 21:42:48 +0800

    changed test1.py

--- test1.py
+++ test1.py
@@ -1,4 +1,16 @@
 from selenium import webdriver
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.page_source
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do',self.browser.title),"browser title was:" + self.browser.title
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
