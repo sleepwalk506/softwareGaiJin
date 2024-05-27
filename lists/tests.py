d169a61d0c3a722463873a35ccb36b9237aea1a5
3150840539@qq.com <3150840539@qq.com>
2024-05-28 00:52:07 +0800

    message 16: from URL of new list by post

--- lists/tests.py
+++ lists/tests.py
@@ -6,15 +6,24 @@ from django.template.loader import render_to_string
 from lists.models import Item
 from django.db import models
 
-
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/new',data={'item_text':'A new list item'})
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+    
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new',data={'item_text':'A new list item'})
+        self.assertRedirects(response,'/lists/the-new-page/')
 
 class HomePageTest(TestCase):
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        response = self.client.get('/')
-        self.assertIn('itemey 1',response.content.decode())
-        self.assertIn('itemey 2',response.content.decode())
+    # def test_displays_all_list_items(self):
+    #     Item.objects.create(text='itemey 1')
+    #     Item.objects.create(text='itemey 2')
+    #     response = self.client.get('/')
+    #     self.assertIn('itemey 1',response.content.decode())
+    #     self.assertIn('itemey 2',response.content.decode())
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -38,24 +47,24 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
 
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/',data={'item_text':'A new list item'})
+    # def test_can_save_a_POST_request(self):
+    #     response = self.client.post('/',data={'item_text':'A new list item'})
 
-        self.assertEqual(Item.objects.count(),1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text,'A new list item')
+    #     self.assertEqual(Item.objects.count(),1)
+    #     new_item = Item.objects.first()
+    #     self.assertEqual(new_item.text,'A new list item')
 
-        # self.assertIn('A new list item',response.content.decode())
-        # self.assertTemplateUsed(response,'home.html')
+    #     # self.assertIn('A new list item',response.content.decode())
+    #     # self.assertTemplateUsed(response,'home.html')
     
-    def test_redirects_after_POST(self):
-        response = self.client.post('/',data={'item_text':'A new list item'})
-        self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/')
+    # def test_redirects_after_POST(self):
+    #     response = self.client.post('/',data={'item_text':'A new list item'})
+    #     self.assertEqual(response.status_code,302)
+    #     self.assertEqual(response['location'],'/lists/the-new-page/')
 
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(),0)
+    # def test_only_saves_items_when_necessary(self):
+    #     self.client.get('/')
+    #     self.assertEqual(Item.objects.count(),0)
 
 
 class ItemModelTest(TestCase):
@@ -77,6 +86,19 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text,'Item the second')
 
 
+
+class ListViewTest(TestCase):
+    def test_use_list_template(self):
+        response = self.client.get('/lists/the-new-page/')
+        self.assertTemplateUsed(response,'list.html')
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-new-page/')
+
+        self.assertContains(response,'itemey 1')
+        self.assertContains(response,'itemey 2')
 # # Create your tests here.
 # class SmokeTest(TestCase):
 #     def test_root_url_resolves_to_home_page_view(self):
--- lists/views.py
+++ lists/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from lists.models import Item
 
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-new-page/')
+
+def view_list(request):
+    items = Item.objects.all()
+    # return render(request,'home.html',{'items':items})
+    return render(request,'list.html',{'items':items})
+
+
 def home_page(request):
     # return HttpResponse('<html><title>To-Do lists</title></html>')
     # return render(request,'home.html')
@@ -23,12 +33,15 @@ def home_page(request):
 
     # return render(request,'home.html',{'new_item_text':request.POST.get('item_text','')})
 
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     # return redirect('/')
+    #     return redirect('/lists/the-new-page/')
     
-    items = Item.objects.all()
+    # items = Item.objects.all()
     # return render(request,'home.html')
-    return render(request,'home.html',{'items':items})
+    # return render(request,'home.html',{'items':items})
+
+    return render(request,'home.html')
     
 # Create your views here.
