7b2c44270fb8b5ceefb7aea7bae138e00195947f
3150840539@qq.com <3150840539@qq.com>
2024-05-27 20:54:39 +0800

    message 7:Refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -9,12 +9,14 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func,home_page)
     
     def test_home_page_return_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        # request = HttpRequest()
+        # response = home_page(request)
+        # html = response.content.decode('utf8')
+        # self.assertTrue(html.startswith('<html>'))
+        # self.assertIn('<title>To-Do lists</title>', html)
+        # self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
 
 # # Create your tests here.
 # class SmokeTest(TestCase):
--- test1.py
+++ test1.py
@@ -48,5 +48,10 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element(By.ID,'id_list_table')
         rows = table.find_elements(By.TAG_NAME,'tr')
         self.assertIn('1:Buy flowers',[row.text for row in rows])
+        
+        time.sleep(5000)
 
         self.fail('Finish the test!')
+
+
+        
