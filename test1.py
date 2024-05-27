7da78dfe75a12f62a07a02d16eb8b07de6633f82
3150840539@qq.com <3150840539@qq.com>
2024-05-27 21:41:33 +0800

    message 8:Before refactoring the functional_tests

--- test1.py
+++ test1.py
@@ -47,8 +47,9 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID,'id_list_table')
         rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1:Buy flowers',[row.text for row in rows])
-        
+        self.assertIn('1: Buy flowers',[row.text for row in rows])
+        self.assertIn('2: Give a gift to Lisi',[row.text for row in rows])
+
         time.sleep(5000)
 
         self.fail('Finish the test!')
